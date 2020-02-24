e0bf4fb4f8c5793cb3b76871f33b05bd936bb965
kiwamu <1979kiwamu@gmail.com>
2020-02-24 22:10:37 +0900

    commit4

--- workspace/first_traning/src/change/Problem01.java
+++ workspace/first_traning/src/change/Problem01.java
@@ -7,7 +7,7 @@ import util.NumericUtil;
 public class Problem01 {
 
     /**
-     * ランダムで生成した数値を振り分ける問題  new-branch3
+     * ランダムで生成した数値を振り分ける問題  new-branch4
      * @param args
      */
     public static void main(String[] args) {
