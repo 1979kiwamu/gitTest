0c775d5388bb07be227f40dba4cc7e9e3ac6e899
kiwamu <1979kiwamu@gmail.com>
2020-02-24 15:40:36 +0900

    変更テスト
    ；；

--- workspace/first_traning/src/change/Problem01.java
+++ workspace/first_traning/src/change/Problem01.java
@@ -7,7 +7,7 @@ import util.NumericUtil;
 public class Problem01 {
 
     /**
-     * ランダムで生成した数値を振り分ける問題
+     * ランダムで生成した数値を振り分ける問題 test
      * @param args
      */
     public static void main(String[] args) {
